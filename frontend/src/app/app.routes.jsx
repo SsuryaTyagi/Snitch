da8734250b5b34009fd46f79b33cc3a95e206b0d
SsuryaTyagi <2040surya@gmail.com>
2026-08-31 22:27:32 +0530

    chore: add comments for clarity in app routes

--- frontend/src/app/app.routes.jsx
+++ frontend/src/app/app.routes.jsx
@@ -3,14 +3,17 @@ import Register from "../features/auth/pages/Register";
 import Login from "../features/auth/pages/Login";
 
 export const router = createBrowserRouter([
+  // home route
   {
     path: "/",
     element: <h1>Home Page</h1>,
   },
+  // register route
   {
     path: "/register",
     element: <Register />,
   },
+  // login route
   {
     path: "login",
     element: <Login />,
