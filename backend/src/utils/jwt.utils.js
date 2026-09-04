7bd39c2653dc42868776e90990087b99fc1ad26a
SsuryaTyagi <2040surya@gmail.com>
2026-09-04 19:59:21 +0530

    chore: add comments for clarity in JWT token utility functions

--- backend/src/utils/jwt.utils.js
+++ backend/src/utils/jwt.utils.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
-
+// Email generate Token function 
 export const generateVerificationToken = (email) => {
   return jwt.sign({ email }, config.JWT_TOKEN_SECRET, { expiresIn: "1d" });
 };
 
+// Email verify token function 
 export const verifyVerificationToken = (token) => {
   return jwt.verify(token, config.JWT_TOKEN_SECRET);
 };
